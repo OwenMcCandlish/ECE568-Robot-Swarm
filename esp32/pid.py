132e2160f16e2d475d8d80cf29c021132deeab46
jjv-cyber <vandaveerjoshua@gmail.com>
2026-04-26 13:12:29 -0400

    Robot code completed

--- esp32/pid.py
+++ esp32/pid.py
@@ -1,35 +1,34 @@
 ##########################################################################################################
 # Imports
 import config
-
+import time
 
 ##########################################################################################################
 # PID Controller for the Velocity Calculations
 # init arguments: goal_pos [x,y], starting_pos [x, y]
 class pid_speed_controller():
-    def __init__(self):
+    def __init__(self, start, end):
         kp = config.MAX_V / config.MAX_E
         kd = 0.2
         ki = (kp**2) / (4*(1+kd))
         self.PID = [kp, ki, kd]
 
-        # previous position and the time it was taken (for velocity calculations)
-        self.prev_position = None
-        self.prev_time_stamp = 0.0
+        self.goal_pos = end
 
+        # previous position and the time it was taken (for velocity calculations)
+        self.prev_position = start
+        self.prev_time_stamp = time.time()
         self.error_sum = 0
 
         # error queue for tracking the sum of errors
         self.error_dq = deque(maxlen=20)
         self.v_max = config.MAX_V
 
-        self.goal_pos = None
-
     #########################################################################################################
     # CALCULATE_DISTANCE:
     def distance(self, point1, point2):
-        delta_y = (point1[1] - point2[1])
         delta_x = (point1[0] - point2[0])
+        delta_y = (point1[1] - point2[1])
         dist = math.sqrt(delta_x**2 + delta_y**2)
         return dist
 
@@ -38,7 +37,7 @@ class pid_speed_controller():
     def pid_calculate(self, pos):
         e = self.distance(self.goal_pos, pos)
         if math.isinf(e):
-            e = config.BOT_LENGTH_CM
+            e = config.BOT_LENGTH_CM * 2
         e_1 = (self.distance(pos, self.prev_position)) / (time.time() - self.prev_time_stamp) # -dx/dt
         V = self.calculate_velocity(e, e_1)
         self.update(e, pos)
@@ -48,7 +47,7 @@ class pid_speed_controller():
     # UPDATE: update the time, error sum, and the previous position (for current Velocity calculation)
     def update(self, e, pos):
         self.error_dq.append(e)                             # add the error to the queue (pop off old values)
-        self.error_sum = sum(self.error_dq)   # error is the sum of 10 most recent errors
+        self.error_sum = sum(self.error_dq)                 # error is the sum of 10 most recent errors
         self.prev_position = pos
         self.prev_time_stamp = time.time()
 
---
+++ main_esp32.py
@@ -0,0 +1,40 @@
+from shared import network as esp_network
+from esp32 import imu, motor_driver
+import config
+import robot
+import time
+
+R_ID = 0       # config the id of the robot
+
+if (R_ID == 0):
+    START_POINT = R0_START
+    END_POINT   = R0_END
+elif (R_ID == 1):
+    START_POINT = R0_START
+    END_POINT   = R0_END
+else (R_ID == 2):
+    START_POINT = R0_START
+    END_POINT   = R0_END
+
+if __name__ == "__main__":
+    # Fetch classes
+    DRIVER = motor_driver.TB6612FNG()
+    NET = esp_network.Esp32Network()
+    ROBOT = robot.robot(DRIVER, START_POINT, END_POINT)
+
+    # initialize
+    NET.start()
+    last_msg = time.time()
+
+    # control loop
+    while (True):
+        # Check if waypoints were sent to the ESP
+        if (NET.poll()):
+            [yaw, _ ], curr_pos, goal_pos = NET.recv().data
+            ROBOT.run(curr_pos, goal_pos, yaw)
+            last_msg = time.time()
+
+        # otherwise if too much time between msgs -> force stop
+        else (time.time() - last_msg) >= config.NET_TIMEOUT:
+            print(f"ERROR. Network timeout")
+            ROBOT.emergency_stop()
